06591bf75b011409db68203045f4de4cb7692896
kondik19 <konrad.c92@gmail.com>
2021-09-29 12:43:36 +0200

    Remove code

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,15 +2,13 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
 
-    val = (Button)findViewById(R.id.changeColorButton);
 
 
